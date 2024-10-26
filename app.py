e93ebbb6001bb43d38318795c02039712485b429
Akash Ghosh <111114759+AXC-Git@users.noreply.github.com>
2024-10-26 17:16:15 +0000

    initial commit of tvshows searcher

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, jsonify
+from cs50 import SQL
+
+db = SQL("sqlite:///shows_title.db")
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/search")
+def search():
+    q = request.args.get("q")
+    if q:
+        shows = db.execute("SELECT * from shows_title where originalTitle LIKE ? AND CAST(startYear AS UNSIGNED) >= 2003 LIMIT 50", "%" + q + "%")
+    else:
+        shows = []
+    return render_template("search.html", shows=shows)
+
